308ed7c2d711be873595118a2118098182b5d367
Kevin-AC <kevin.argumedo@gmail.com>
2023-06-01 20:54:13 -0500

    feat: added product in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 // import Login from './pages/login/Login.jsx'
-import AddProduct from './pages/AddProduct/AddProduct'
+// import AddProduct from './pages/AddProduct/AddProduct'
+// import AddInventory from './pages/AddInventory/AddInventory'
+import Product from './pages/Product/Product'
 function App () {
   return (
-      <main className='font-Poppins w-screen min-h-screen bg-background grid place-items-center'>
+      <main className='font-Poppins w-full min-h-screen px-20 bg-background grid place-items-center gap-20'>
         {/* <Login></Login> */}
-        <AddProduct></AddProduct>
+        {/* <AddProduct></AddProduct> */}
+        {/* <AddInventory></AddInventory> */}
+        <Product></Product>
       </main>
   )
 }
