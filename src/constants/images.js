99a0a55faddbd9ec49cc86d847fc101a1c4408cf
Denis Herrera Espiritu <jsherr.6@gmail.com>
2023-06-02 11:17:06 -0500

    chrore: create folder for organizing images

---
+++ src/constants/images.js
@@ -0,0 +1,15 @@
+import eyeicon from '../assets/eye-line.png'
+import addicon from '../assets/ri_add-circle-line.png.png'
+import deleticon from '../assets/ri_delete-bin-2-line.png'
+import rieyeicon from '../assets/ri_eye-line.png'
+import lockicon from '../assets/ri_lock-password-line.png'
+import editicon from '../assets/Vector.png'
+
+export default {
+  eyeicon,
+  addicon,
+  deleticon,
+  rieyeicon,
+  lockicon,
+  editicon
+}
