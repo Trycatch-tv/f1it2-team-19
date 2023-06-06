7f4e5f7cf29a9c7abdf3a6a9d1d72c6db0df6527
Kevin-AC <kevin.argumedo@gmail.com>
2023-06-06 17:40:39 -0500

    chore: path edit icon and new record botton

--- src/components/NewRecord.jsx
+++ src/components/NewRecord.jsx
@@ -1,9 +1,14 @@
+
+import { Link } from 'react-router-dom'
 import { images } from '../constants'
-export default function NewRecord () {
+// eslint-disable-next-line react/prop-types
+export default function NewRecord ({ route }) {
   return (
-    <button className="w-[186px] h-10 bg-primary text-soft_color rounded-lg2 flex gap-4 items-center justify-center">
-      <img src={images.addicon} alt="Boton Agregar" />
-      Nuevo Registro
-    </button>
+   <Link to={route}>
+      <button className="w-[186px] h-10 bg-primary text-soft_color rounded-lg2 flex gap-4 items-center justify-center">
+        <img src={images.addicon} alt="Boton Agregar" />
+        Nuevo Registro
+      </button>
+   </Link>
   )
 }
