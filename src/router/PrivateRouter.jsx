0a4716915176abc674cdf7e5ec7d33ad35171cff
Kevin-AC <kevin.argumedo@gmail.com>
2023-06-06 17:47:58 -0500

    chore: disable authentication

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -2,9 +2,7 @@ import { Navigate } from 'react-router-dom'
 import DashboardLayout from '../layout/DashboardLayout'
 
 const PrivateRoute = () => {
-  const auth = {
-    // dev: 'code'
-  }
+  const auth = { nombre: 'juan' }
 
   const value = Object.entries(auth).length !== 0
   return (
